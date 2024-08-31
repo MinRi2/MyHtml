fa096d1aa677eb84995dd8d6648cb663314339d7
MinRi2 <2275045670@qq.com>
2024-08-31 21:42:37 +0800

    Lazy Mount

---
+++ src/scripts/components/LazyMount.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const props = defineProps<{
+    delaySeconds: string
+}>();
+
+const initSuccess = ref(false);
+
+onMounted(() => {
+    setTimeout(() => initSuccess.value = true, parseInt(props.delaySeconds) * 1000);
+});
+</script>
+
+<template>
+    <span>
+        <slot v-if="initSuccess"></slot>
+    </span>
+</template>
